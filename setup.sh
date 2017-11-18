eafe986b46a00c4613821ecc62ae075c627a9724
izcet <isaacjrhett@gmail.com>
2017-11-18 15:30:22 -0800

    [setup.sh] +[packages.txt]

--- setup.sh
+++ setup.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
-
 # must be run as root
-apt-get install -y git screen at vim openssh-server gcc clang htop valgrind nmap texlive gzip wireshark cpp ltrace smartmontools
-emacs expect krb5-kdc krb5-locales zsh
-cp ./.vimrc ~/.vimrc
+
+FILE="packages.txt"
+
+# for each line
+while read line ; do
+
+	# ignore lines that are #commented
+	PKG=$(echo $line | cut -d'#' -f1)
+
+	if [ -n "$PKG" ] ; then
+		apt-get install -y $PKG
+	else
+		# Don't print for empty lines
+		if [ -n "$line" ] ; then
+			echo "Skipping '$line'"
+		fi
+	fi
+
+done < $FILE
+
+# TODO
 # set up prettyvim
+# copy .rc files (vim, zsh, bash, etc)
+# install fish
+# install gitpushplus
 
 # add alias functions to .bashrc
